328ca09f973a723d2064c3d12cf36b3696385b04
anshull2 <itsanshulchoudhary2006@gmail.com>
2026-03-27 15:45:17 +0530

    Implement palindrome checker in UseCase2PalindromeCheckerApp
    Added a class to check if a hardcoded string is a palindrome.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+/**
+ * UseCase2PalindromeCheckerApp
+ *
+ * This class checks whether a hardcoded string is a palindrome
+ * and prints the result.
+ *
+ * @author Anshul
+ * @version 2.0
+ */
 
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
